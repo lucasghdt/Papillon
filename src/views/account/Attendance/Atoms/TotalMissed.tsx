243651d7d8ed1d9433f7302d74f440fb8874a3ec
Mael <96339570+ryzenixx@users.noreply.github.com>
2025-05-12 01:29:49 +0200

    fix: plural for missed/unjustified absences

--- src/views/account/Attendance/Atoms/TotalMissed.tsx
+++ src/views/account/Attendance/Atoms/TotalMissed.tsx
@@ -70,7 +70,7 @@ const TotalMissed = ({ totalMissed }: TotalMissedProps) => {
               color: theme.colors.text + "88",
             }}
           >
-            manquées
+            {totalMissed.total.hours >= 2 ? "manquées" : "manquée"}
           </NativeText>
         </View>
         <View
@@ -106,10 +106,10 @@ const TotalMissed = ({ totalMissed }: TotalMissedProps) => {
           </View>
           <NativeText
             style={{
-              color: (totalMissed.unJustified.hours > 0) ? "#D10000" : theme.colors.text + "88",
+              color: theme.colors.text + "88",
             }}
           >
-            injustifiées
+            {totalMissed.unJustified.hours >= 2 ? "injustifiées" : "injustifiée"}
           </NativeText>
         </View>
       </View>
